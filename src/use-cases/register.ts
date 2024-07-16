65ffffcdd11b3e8b33bc107235356786ec470b8a
LuisLoschi <gustavoloschi362@gmail.com>
2024-07-16 00:12:02 -0300

    new: creating repository interface

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@/lib/prisma'
+import { UserRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
@@ -8,18 +8,14 @@ interface RegisterUseCaseRequest {
 }
 
 export class RegisterUseCase {
-  constructor(private userRepository: any){
+  constructor(private userRepository: UserRepository){
 
   }
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
     const password_hash = await hash(password, 6)
   
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.userRepository.findByEmail(email)
   
     if (userWithSameEmail) {
       throw new Error('Email alredy exist.')
@@ -33,6 +29,5 @@ export class RegisterUseCase {
       password_hash,
     })
   }
-
 }
 
