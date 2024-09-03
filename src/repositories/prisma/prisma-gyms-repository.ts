872cf011316afe3a3902025fc114c02623e74462
LuisLoschi <gustavoloschi362@gmail.com>
2024-09-03 13:51:46 -0300

    new: prisma gym and check-in repositories

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,50 @@
+import { prisma } from '@/lib/prisma'
+import { Gym, Prisma } from '@prisma/client'
+import { FindManyNearbyParams, GymRepository } from '../gym-repository'
+
+export class PrismaGymsRepository implements GymRepository {
+
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id
+      }
+    })
+
+    return gym
+  }
+  
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+
+
+  async searchManygyms(search: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: search,
+        }
+      },
+      skip: (page - 1) * 20,
+      take: 20
+    })
+
+    return gyms
+  }
+
+
+  async findManyNearby({ latitude, longitude }: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+}
+
